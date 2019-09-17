d481f80b795574886cbbe8c62997f833ea7ebf55
abhaydhiraj <abhay@abjen1691.us-central1-a.c.abhay1609.internal>
2019-09-17 13:07:14 +0000

    updated for rolling

--- ecommerce/src/main/java/hello/MainController.java
+++ ecommerce/src/main/java/hello/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 	public @ResponseBody User getAllUsers() {
 		// This returns a JSON or XML with the users
 		User u = new User();
-		u.setName("Updated to punit abhay");
+		u.setName("Rolling update by abhay");
 		u.setEmail("abhay@gmail.com");
 //		return userRepository.findAll();
 		return u;
