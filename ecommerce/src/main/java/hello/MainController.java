0fc3afd5b69db2e0880b81446814e770b6063f79
abhaydhiraj <abhay@abjen1691.us-central1-a.c.abhay1609.internal>
2019-09-18 05:43:01 +0000

    chenges to asushus

--- ecommerce/src/main/java/hello/MainController.java
+++ ecommerce/src/main/java/hello/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 	public @ResponseBody User getAllUsers() {
 		// This returns a JSON or XML with the users
 		User u = new User();
-		u.setName("KUmar abhay"");
+		u.setName("KUmar abhay waaaaaaaaaaaaaaaaaaaaaaaahhhhh");
 		u.setEmail("abhay@gmail.com");
 //		return userRepository.findAll();
 		return u;
