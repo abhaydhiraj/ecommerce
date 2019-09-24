412eb12d195777a76877b0fc2d196f0cc3ad2242
abhaydhiraj <abhaydhiraj@gmail.com>
2019-09-24 16:07:05 +0530

    Update MainController.java

--- ecommerce/src/main/java/hello/MainController.java
+++ ecommerce/src/main/java/hello/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 	public @ResponseBody User getAllUsers() {
 		// This returns a JSON or XML with the users
 		User u = new User();
-		u.setName("I love you babyyyyyyyyyyyy :)");
+		u.setName("Hello punit and abhay :)");
 		u.setEmail("abhay@gmail.com");
 //		return userRepository.findAll();
 		return u;
